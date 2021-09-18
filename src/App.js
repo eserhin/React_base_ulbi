c21815e93ceee039274fca83d2687e53a1bfa690
eserhin <47678732+eserhin@users.noreply.github.com>
2021-09-19 01:09:15 +0300

    Axios

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useMemo, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './styles/App.css'
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
@@ -6,30 +6,40 @@ import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/MyModal/MyModal";
 import MyButton from "./components/UI/button/MyButton";
 import {usePosts} from "./hooks/usePosts";
+import PostService from "./API/PostService";
+import Loader from "./components/UI/Loader";
 
 function App() {
-    const [posts, setPosts] = useState([
-        {id: '1', title: 'JS1', body: "Post number1"},
-        {id: '2', title: 'JS2', body: "Post number2"},
-        {id: '3', title: 'JS3', body: "Post number3"}
-    ]);
+    const [posts, setPosts] = useState([]);
 
     const [filter, setFilter] = useState({sort: '', query: ''});
-
     const [modal, setModal] = useState(false);
     const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+    const [isPostsLoading, setIsPostsLoading] = useState(false);
+
+    useEffect(() => {
+        fetchPosts()
+    }, []);
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
         setModal(false);
     };
+
+    async function fetchPosts() {
+        setIsPostsLoading(true);
+        const posts = await PostService.getAll();
+        setPosts(posts);
+        setIsPostsLoading(false);
+    }
+
     const removePost = (post) => {
         setPosts(posts.filter(p => p.id !== post.id))
     };
 
     return (
         <div className='App'>
-            <MyButton style = {{marginTop: 30}} onClick = {()=>setModal(true)}>
+            <MyButton style={{marginTop: 30}} onClick={() => setModal(true)}>
                 Создать пользователя
             </MyButton>
             <MyModal visible={modal} setVisible={setModal}>
@@ -37,7 +47,8 @@ function App() {
             </MyModal>
             <hr/>
             <PostFilter filter={filter} setFilter={setFilter}/>
-            <PostList posts={sortedAndSearchedPosts} title="Список постов 1" remove={removePost}/>
+            {isPostsLoading ? <div style={{display: 'flex', justifyContent: 'center', marginTop: 50}}><Loader/></div> :
+                <PostList posts={sortedAndSearchedPosts} title="Список постов 1" remove={removePost}/>}
         </div>
     );
 }
